8911a42d96a563e8213ea5650206326e0e409ba3
leeseobin00 <leeseobin000709@gmail.com>
2024-01-13 02:37:13 +0900

    ♻️[refactor]: gender 입력 추가

--- src/main/java/com/eureuni/eureunibe/domain/user/usecase/UserSignup.java
+++ src/main/java/com/eureuni/eureunibe/domain/user/usecase/UserSignup.java
@@ -1,7 +1,8 @@
 package com.eureuni.eureunibe.domain.user.usecase;
 
+import com.eureuni.eureunibe.domain.user.domain.Gender;
 import com.eureuni.eureunibe.global.jwt.presentation.JwtResponse;
 
 public interface UserSignup {
-    JwtResponse execute(String email, String password, String nickname);
+    JwtResponse execute(String email, String password, String nickname, Gender gender);
 }
