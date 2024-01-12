7d8201e5f713e62f9b1285be8628aad9d56c7f27
leeseobin00 <leeseobin000709@gmail.com>
2024-01-12 22:50:41 +0900

    ✨[feat]: JwtFilter에서 resolveToken 메소드 추가

---
+++ src/main/java/com/eureuni/eureunibe/global/jwt/JwtFilter.java
@@ -0,0 +1,24 @@
+package com.eureuni.eureunibe.global.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+    private final JwtProvider jwtProvider;
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer")) {
+            return bearerToken.split(" ")[1];
+        }
+        return null;
+    }
+}
