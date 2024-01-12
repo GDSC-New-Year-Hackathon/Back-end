3a7b71015f20455656f4f70a6f725ce605cbb03b
leeseobin00 <leeseobin000709@gmail.com>
2024-01-13 01:38:42 +0900

    🔧[chore]: UserInfoRequest 클래스 어노테이션 추가

--- src/main/java/com/eureuni/eureunibe/domain/user/dto/UserInfoRequest.java
+++ src/main/java/com/eureuni/eureunibe/domain/user/dto/UserInfoRequest.java
@@ -2,7 +2,11 @@ package com.eureuni.eureunibe.domain.user.dto;
 
 import com.eureuni.eureunibe.domain.user.domain.Gender;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Getter;
 
+@Getter
+@Builder
 public class UserInfoRequest {
     @NotBlank
     private Gender gender;
