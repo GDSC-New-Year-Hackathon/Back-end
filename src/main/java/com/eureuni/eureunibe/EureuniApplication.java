5fe2dfc94c62d667354fa622bc16733dba94f6bd
leeseobin00 <leeseobin000709@gmail.com>
2024-01-12 21:43:52 +0900

    🎉[feat]: 프로젝트 초기 설정

---
+++ src/main/java/com/eureuni/eureunibe/EureuniApplication.java
@@ -0,0 +1,13 @@
+package com.eureuni.eureunibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EureuniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EureuniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eureuni/eureunibe/EureuniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureuni.eureunibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EureuniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
