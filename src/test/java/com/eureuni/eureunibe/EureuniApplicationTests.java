de14032d8b7e2c2b96628c76d90ed108780fe85f
jiinkyung <jinkyung010310@gmail.com>
2024-01-13 01:09:12 +0900

    🔨[chore]: test 어노테이션 삭제

--- src/test/java/com/eureuni/eureunibe/EureuniApplicationTests.java
+++ src/test/java/com/eureuni/eureunibe/EureuniApplicationTests.java
@@ -3,7 +3,7 @@ package com.eureuni.eureunibe;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class EureuniApplicationTests {
 
 	@Test
