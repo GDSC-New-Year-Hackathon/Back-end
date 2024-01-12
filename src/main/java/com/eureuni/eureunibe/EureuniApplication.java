1ba0f023a2ee430477756b9b1db77363ab3752d0
leeseobin00 <leeseobin000709@gmail.com>
2024-01-13 00:56:17 +0900

    ✨[feat]: @EnableJpaAuditing 추가

--- src/main/java/com/eureuni/eureunibe/EureuniApplication.java
+++ src/main/java/com/eureuni/eureunibe/EureuniApplication.java
@@ -2,12 +2,14 @@ package com.eureuni.eureunibe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class EureuniApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EureuniApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EureuniApplication.class, args);
+    }
 
 }
