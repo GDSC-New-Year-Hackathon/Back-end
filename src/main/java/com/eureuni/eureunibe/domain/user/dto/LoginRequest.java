7e515d0662538493a11643ff1c31bbb1bd9eba85
leeseobin00 <leeseobin000709@gmail.com>
2024-01-13 01:38:24 +0900

    🔧[chore]: LoginRequest 클래스 어노테이션 추가

--- src/main/java/com/eureuni/eureunibe/domain/user/dto/LoginRequest.java
+++ src/main/java/com/eureuni/eureunibe/domain/user/dto/LoginRequest.java
@@ -2,7 +2,11 @@ package com.eureuni.eureunibe.domain.user.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Getter;
 
+@Getter
+@Builder
 public class LoginRequest {
     @NotBlank
     @Email
