8f3899f2e5ea3a26b5efa9fe7f38ee335e94c70c
leeseobin00 <leeseobin000709@gmail.com>
2024-01-13 00:43:15 +0900

    🔧[chore]: 폴더명 변경

--- src/main/java/com/eureuni/eureunibe/domain/shared/entity/BaseTimeEntity.java
+++ src/main/java/com/eureuni/eureunibe/domain/shared/entity/BaseTimeEntity.java
@@ -1,4 +1,4 @@
-package com.eureuni.eureunibe.domain.domain;
+package com.eureuni.eureunibe.domain.shared.entity;
 
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
