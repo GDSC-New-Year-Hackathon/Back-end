1407093ef2f2f7059826e6661e1c3accec9d92c6
leeseobin00 <leeseobin000709@gmail.com>
2024-01-13 01:45:18 +0900

    ✨[feat]: User 관련 ErrorCode 추가

--- src/main/java/com/eureuni/eureunibe/global/error/ErrorCode.java
+++ src/main/java/com/eureuni/eureunibe/global/error/ErrorCode.java
@@ -10,7 +10,9 @@ public enum ErrorCode {
     INVALID_TOKEN(401, "INVALID_TOKEN", "유효하지 않은 토큰입니다."),
     EXPIRED_TOKEN(403, "EXPIRED_TOKEN", "만료된 토큰입니다."),
     USER_NOT_FOUND(404,"USER_NOT_FOUND","유저를 찾을 수 없습니다."),
-    COIN_NOT_FOUND(404,"COIN_NOT_FOUND","코인을 찾을 수 없습니다.");
+    EMAIL_DUPLICATE(409, "EMAIL_DUPLICATE", "중복된 이메일입니다."),
+    PASSWORD_NOT_MATCH(400, "PASSWORD_NOT_MATCH", "비밀번호가 일치하지 않습니다.");
+
     private final int httpStatus;
     private final String code;
     private final String message;
